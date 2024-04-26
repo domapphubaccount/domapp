a0310c728ca8fd2f39c605fbadad2d815f36efef
Abdulrahman Mohammed <109548830+AbdulrahmanM0@users.noreply.github.com>
2024-04-26 09:22:16 +0300

    update erp system

--- src/pages_sups/Products/ERP/Bannar.jsx
+++ src/pages_sups/Products/ERP/Bannar.jsx
@@ -7,7 +7,7 @@ import Erp_Dash from "@/assets/images/Products_Page/Erp/ERP_dash.png"
 export default function Product({bannar,title,caption}) {
   return (
     <section className='erp_bannar'>
-            <div className='products' style={{background: `#3f4448 url(${Drop.src})`}}>
+            <div className='products'>
             {/* <Image src={bannar} alt='product' className="bannar" /> */}
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className='chat_wave'>
                     <path fill="#ffffff" fill-opacity="1" d="M0,288L120,293.3C240,299,480,309,720,277.3C960,245,1200,171,1320,133.3L1440,96L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path>
@@ -34,6 +34,7 @@ export default function Product({bannar,title,caption}) {
                 </div>
                 </div>
             </div>
+            <div className='dark_bannar'></div>
         </div>
     </section>
   )
