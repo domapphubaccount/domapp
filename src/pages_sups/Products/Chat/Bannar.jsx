ad353b7b2d79173b9f2f4bc522af8d55bea45aae
Abdulrahman Mohammed <109548830+AbdulrahmanM0@users.noreply.github.com>
2024-04-26 09:08:04 +0300

    update chate b1

--- src/pages_sups/Products/Chat/Bannar.jsx
+++ src/pages_sups/Products/Chat/Bannar.jsx
@@ -10,7 +10,8 @@ export default function Bannar({bannar,title,caption}) {
 
   return (
     <section className='chat_bannar' >
-            <div className={`products ${league.className}`} style={{background:'#1c3f39'}}>
+            <div className={`products ${league.className}`}>
+                <div className='dark_bannar'></div>
                 <svg xmlns="http://www.w3.org/2000/svg" className='chat_wave' viewBox="0 0 1440 320">
                     <path fill="#ffffff" fill-opacity="1" d="M0,128L48,144C96,160,192,192,288,176C384,160,480,96,576,101.3C672,107,768,181,864,202.7C960,224,1056,192,1152,160C1248,128,1344,96,1392,80L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
                 </svg>
@@ -30,6 +31,7 @@ export default function Bannar({bannar,title,caption}) {
                                 </a>
                             </div>
                         </div>
+                    
             </div>
     </section>
   )
