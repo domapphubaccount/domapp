1b9e3e35f3465c94f86251b6e4ad6eb702a7bba0
Abdulrahman Mohammed <109548830+AbdulrahmanM0@users.noreply.github.com>
2024-05-21 21:34:00 +0300

    update lang

--- src/components/Layout/Header/LNG/Lang.jsx
+++ src/components/Layout/Header/LNG/Lang.jsx
@@ -1,39 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import 'flag-icons/css/flag-icons.min.css';
 
 function Lang() {
   const [selectedLang, setSelectedLang] = useState('EN');
+  const [langToggle, setLangToggle] = useState(false);
+  const langRef = useRef(null);
 
   const handleChange = (event) => {
-    const selectedValue = event.target.value;
-    if (selectedValue === 'EN') {
-      setSelectedLang('EN');
-    } else if (selectedValue === 'AR') {
-      setSelectedLang('AR');
-    }
+    const selectedValue = event.target.id;
+    setSelectedLang(selectedValue);
+    setLangToggle(false); // Close the dropdown after selection
   };
 
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (langRef.current && !langRef.current.contains(event.target)) {
+        setLangToggle(false);
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [langRef]);
+
   return (
-    <div className="lngg">
+    <div className='lngg' ref={langRef}>
       <i className="bi bi-globe-americas"></i>
-      <select
-        className="lang-selector text-center"
-        value={selectedLang}
-        onChange={handleChange}
-      >
-        <option value="EN" className='text-center' disabled hidden>
-          {selectedLang}
-        </option>
-        <option value="AR" className='text-center' disabled hidden>
-          {selectedLang}
-        </option>
-        <option value="EN" className='text-center' style={{ fontSize: '.8rem', padding: '0.5rem' }}>
-          English
-        </option>
-        <option value="AR" className='text-center' style={{ fontSize: '.8rem', padding: '2rem' }}>
-          Arabic
-        </option>
-      </select>
+      <div className="lang-selector text-center" onClick={() => setLangToggle(!langToggle)}>
+        {selectedLang}
+        <i className="bi bi-chevron-down"></i>
+      </div>
+      {langToggle &&
+        <div className='position-absolute w-100' style={{ top: '30px' }}>
+          <div className='m-0 p-0 w-100 bg-light'>
+            <div className='m-0 py-2 text-center lng-option' id="EN" onClick={handleChange}>
+              English
+            </div>
+            <div className='m-0 py-2 text-center lng-option' id="AR" onClick={handleChange}>
+              Arabic
+            </div>
+          </div>
+        </div>
+      }
     </div>
   );
 }
