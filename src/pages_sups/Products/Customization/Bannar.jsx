6b00239ddd40e7b385b0ddfad0f9d58b4e9255a0
Abdulrahman Mohammed <109548830+AbdulrahmanM0@users.noreply.github.com>
2024-07-03 16:35:09 +0300

    add slogan

--- src/pages_sups/Products/Customization/Bannar.jsx
+++ src/pages_sups/Products/Customization/Bannar.jsx
@@ -22,7 +22,7 @@ export default function Bannar() {
                                 <h1 className="title theme-gradient"
                                 >Grasshopper®<br/>3D   
                                 </h1>
-                                <p className='text-white'>Precision engineering for exquisite facade fabrication.</p>
+                                <p className='text-white'>Flexible, Intuitive, and powerful dynamic design environment.</p>
                             </div>
                         </div>
                     </div>
