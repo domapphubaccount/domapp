77fccbc28918ab776f5a9aabb413059bde3c6ee4
Abdulrahman Mohammed <109548830+AbdulrahmanM0@users.noreply.github.com>
2024-07-03 18:53:42 +0300

    refactor sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -10,6 +10,9 @@ import {
   ListItemPrefix,
   ListItemSuffix,
   Chip,
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
 } from "@material-tailwind/react";
 import Link from "next/link";
 import Logo from "../../assets/images/Header/dark-logo.png";
@@ -20,11 +23,40 @@ export default function Sidebar({items}) {
   const [open, setOpen] = React.useState(false);
   const openDrawer = () => setOpen(true);
   const closeDrawer = () => setOpen(false);
+  const [openAccordion, setOpenAccordion] = React.useState(0);
+ 
+  const handleOpen = (value) => setOpenAccordion(openAccordion === value ? 0 : value);
+ 
   const navigate = useRouter()
 
   const handleNavigate = (link) => {
     navigate.push(link)
   }
+
+
+  function Icon({ id, open }) {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={2}
+        stroke="currentColor"
+        className={`${id === open ? "rotate-180" : ""} h-5 w-5 transition-transform`}
+      >
+        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+      </svg>
+    );
+  }
+
+  const idItems = [
+    <li>
+    <Link href="/#whatwedo" className='nav-link-item text-black no-underline	' shallow>WHAT WE DO</Link>
+    </li>,
+    <li>
+        <Link href="/#about" className='nav-link-item text-black no-underline	' shallow>ABOUT US</Link>
+    </li>
+  ]
  
   return (
     <React.Fragment>
@@ -52,31 +84,36 @@ export default function Sidebar({items}) {
           </IconButton>
         </div>
         <List>
-            {items.map((item,index)=>(
+
+
+        <Accordion open={openAccordion === 2} icon={<Icon id={2} open={openAccordion} />}>
+          <AccordionHeader onClick={() => handleOpen(2)} className="p-2">
+            Products
+          </AccordionHeader>
+          <AccordionBody>
+          {items.map((item,index)=>(
 
           <ListItem key={index} onClick={()=>handleNavigate(item.link)}>
             <ListItemPrefix>
-              {/* <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="h-5 w-5 mx-2"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M2.25 2.25a.75.75 0 000 1.5H3v10.5a3 3 0 003 3h1.21l-1.172 3.513a.75.75 0 001.424.474l.329-.987h8.418l.33.987a.75.75 0 001.422-.474l-1.17-3.513H18a3 3 0 003-3V3.75h.75a.75.75 0 000-1.5H2.25zm6.04 16.5l.5-1.5h6.42l.5 1.5H8.29zm7.46-12a.75.75 0 00-1.5 0v6a.75.75 0 001.5 0v-6zm-3 2.25a.75.75 0 00-1.5 0v3.75a.75.75 0 001.5 0V9zm-3 2.25a.75.75 0 00-1.5 0v1.5a.75.75 0 001.5 0v-1.5z"
-                  clipRule="evenodd"
-                />
-              </svg> */}
               <div className="flex">
                 <div className="mx-2">{item.icon}</div>
                 <div>{item.title}</div>
               </div>
             </ListItemPrefix>
-
-
           </ListItem>
-        ))}
+          ))}
+          </AccordionBody>
+        </Accordion>
+        {idItems.map((item,index)=>(
+          <ListItem key={index} onClick={()=>handleNavigate(item.link)}>
+            <ListItemPrefix>
+              {item}
+            </ListItemPrefix>
+          </ListItem>
+          ))}
+
+
+
 
 
 
