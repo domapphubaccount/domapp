97939ad176227a08720ed5cee151868f2ac8437a
Abdulrahman Mohammed <109548830+AbdulrahmanM0@users.noreply.github.com>
2024-07-12 19:20:09 +0300

    update clad

--- src/pages_sups/Products/Clad/Bannar.jsx
+++ src/pages_sups/Products/Clad/Bannar.jsx
@@ -8,6 +8,8 @@ import cladWhiteHouse from "@/assets/images/Products_Page/Clad/bannar.jpg"
 import CladBannar from "@/assets/images/Products_Page/Clad/Logo_Bannar.png"
 import objectDrop from "@/assets/images/Products_Page/Clad/object_drop.png"
 import Image from 'next/image'
+import cladback from "@/assets/images/Products_Page/Clad/cladback.jpeg"
+import SVGS from "@/assets/images/Products_Page/Clad/Sprinkle.svg"
 
 export default function Bannar() {
 
@@ -16,7 +18,8 @@ export default function Bannar() {
         <div className='products2'>
             <div className='light-purple-bannar'></div> 
                 <Container>
-                    <img src={cladWhiteHouse.src} alt='products2' className="bannar" />
+                    {/* <img src={cladWhiteHouse.src} alt='products2' className="bannar" /> */}
+                        <img src={SVGS.src} alt='' className='cladback' />
                     <div className="mil-background-grid mil-top-space"></div>
                     <div className="mil-banner-content">
                         <div className="row align-items-end">
@@ -51,7 +54,7 @@ export default function Bannar() {
                                                     190K+
                                                 </span>
                                             </h4>
-                                            <p >
+                                            <p className='fw-bold'>
                                             square meter
                                             <br/> of 
                                             Cladding produced  
@@ -65,7 +68,7 @@ export default function Bannar() {
                                         18K+
                                     </span>
                                 </h4>
-                                <p >
+                                <p className='fw-bold'>
                                     Reduced <br/>Working Hours
                                 </p>
                             </div>
@@ -75,7 +78,7 @@ export default function Bannar() {
                                 <h4 className="mil-accent mil-mb-10">
                                     <span className="mil-counter clad-text-color fw-bold" data-number="99">100+</span>
                                 </h4>
-                                <p >Users <br/> <div className='pt-2 pb-3'></div></p>
+                                <p className='fw-bold'>Users <br/> <div className='pt-2 pb-3'></div></p>
                             </div>
                         </div>
                     </div>
