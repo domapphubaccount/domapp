13f43d5ebf89fc4465bf6cb7a8dfd063285798e8
Abdulrahman Mohammed <109548830+AbdulrahmanM0@users.noreply.github.com>
2024-08-22 13:57:31 +0300

    remove title

--- src/components/Reuse/Section_Head/Video.jsx
+++ src/components/Reuse/Section_Head/Video.jsx
@@ -24,7 +24,7 @@ function Video({title , body , src , className , bannar , additional}) {
                 <div className="relative">
                   <div class={ `${className} absolute -top-4 -bottom-4 -right-4 w-2/3 rounded-3xl z-0 sm:rounded-[50px] md:top-0 md:bottom-0 md:right-0 xl:w-7/12`}></div>
                   <div class="relative">
-                    <div title="Magical Scotland - 4K Scenic Relaxation Film with Calming Music">
+                    <div >
                       <div className="relative video-screen-border">
                         <img
                           className="w-full aspect-video rounded-2xl"
