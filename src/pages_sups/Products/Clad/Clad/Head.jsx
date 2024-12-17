1b0106ea2c5dfe1989275cf250d3955c735a9b9a
Abdulrahman Mohammed <109548830+AbdulrahmanM0@users.noreply.github.com>
2024-12-17 15:08:29 +0200

    fixing windmaster name on header

--- src/pages_sups/Products/Clad/Clad/Head.jsx
+++ src/pages_sups/Products/Clad/Clad/Head.jsx
@@ -1,44 +1,90 @@
-"use client"
-import React from 'react'
-import { Container } from 'reactstrap'
-import quotes from "@/assets/images/Products_Page/Rd/quotes-left.svg"
-import Image from 'next/image'
+"use client";
+import React from "react";
+import { Container } from "reactstrap";
+import quotes from "@/assets/images/Products_Page/Rd/quotes-left.svg";
+import Image from "next/image";
 import dynamic from "next/dynamic";
-import { useSelector } from 'react-redux'
+import { useSelector } from "react-redux";
 
 function Head({ head }) {
   const { lang, dir } = useSelector((state) => state.languageSlice);
   const { cladcut } = useSelector((state) => state.cladcutRed);
 
-
   return (
-    <section className='py-5 mt-5'>
+    <section className="py-5 mt-5">
       <Container>
         <div dir={dir}>
-          <div className='text-start' style={{ width: '80px' }} data-aos="fade-up" data-aos-duration="500">
-            <Image src={quotes.src} width={80} height={80} className='me-auto' alt="quote" />
+          <div
+            className="text-start"
+            style={{ width: "80px" }}
+            data-aos="fade-up"
+            data-aos-duration="1000"
+          >
+            <img src={quotes.src} className="me-auto" alt="qute" />
           </div>
-          <div className='text-center' data-aos="fade-up" data-aos-duration="2000">
+          <div
+            className="text-center"
+            data-aos="fade-up"
+            data-aos-duration="2000"
+          >
             <Container>
               <div className={"mil-center text-center mil-clad"}>
-                <span className="mil-suptitle mil-upper" style={{ translate: "none", rotate: "none", scale: "none", transform: "translate(0px, 0px)", opacity: "1" }}>
+                <span
+                  className="mil-suptitle mil-upper"
+                  style={{
+                    translate: "none",
+                    rotate: "none",
+                    scale: "none",
+                    transform: "translate(0px, 0px)",
+                    opacity: "1",
+                  }}
+                >
                   {head}
                 </span>
-                <h2 className="mil-up mil-mb-30" style={{ translate: "none", rotate: "none", scale: "none", transform: "translate(0px, 0px)", opacity: "1" }}>
+                <h2
+                  className="mil-up mil-mb-30"
+                  style={{
+                    translate: "none",
+                    rotate: "none",
+                    scale: "none",
+                    transform: "translate(0px, 0px)",
+                    opacity: "1",
+                  }}
+                >
                   {cladcut(lang).sections.Head_NOTE_1.title}
                 </h2>
-                <p className={"mil-up"} style={{ translate: "none", rotate: "none", scale: "none", transform: "translate(0px, 0px)", opacity: 1 }}>
+                <p
+                  className={"mil-up"}
+                  style={{
+                    translate: "none",
+                    rotate: "none",
+                    scale: "none",
+                    transform: "translate(0px, 0px)",
+                    opacity: 1,
+                  }}
+                >
                   {cladcut(lang).sections.Head_NOTE_1.slogan}
                 </p>
               </div>
             </Container>
           </div>
-          <div className='text-end' data-aos="fade-up" data-aos-duration="500">
-            <Image src={quotes.src} width={80} height={80} className="ms-auto" alt="quote" />
+          <div
+            className="flex justify-end"
+            // style={{ width: "80px" }}
+            data-aos="fade-up"
+            data-aos-duration="1000"
+
+          >
+            <img
+              src={quotes.src}
+              style={{ width: "80px" }}
+              className="inline"
+              alt="qute"
+            />
           </div>
         </div>
       </Container>
     </section>
-  )
+  );
 }
-export default dynamic (() => Promise.resolve(Head), {ssr: false})
+export default dynamic(() => Promise.resolve(Head), { ssr: false });
