79b8ed06fd504d552589d7bd1906a80f034ac0a9
israaboelfotouh <esraaaboelfotouh14@gmail.com>
2025-11-05 11:00:04 +0200

    Remove bg "tittle"

--- src/pages_sups/Products/BondifyCRM/BondifyCRM/Break.jsx
+++ src/pages_sups/Products/BondifyCRM/BondifyCRM/Break.jsx
@@ -3,10 +3,10 @@ import { Container } from "reactstrap";
 
 export default function Break({body}) {
   return (
-    <section className="erp_section_3 mt-5">
+    <section className=" mt-5">
       <Container>
         <div className="text-center fw-bold">
-          {body}
+          {body} 
         </div>
       </Container>
     </section>
