3525643f3673e3e8983ee306b1df377a68d256c6
israaboelfotouh <esraaaboelfotouh14@gmail.com>
2025-11-05 16:43:52 +0200

    section4

--- src/app/bondifycrm/Page_sup/Page_contant.jsx
+++ src/app/bondifycrm/Page_sup/Page_contant.jsx
@@ -39,6 +39,7 @@ import { useSelector } from "react-redux";
 import Section1 from "@/pages_sups/Products/BondifyCRM/NewSections/Section1";
 import Section2 from "@/pages_sups/Products/BondifyCRM/NewSections/Section2";
 import Section3 from "@/pages_sups/Products/BondifyCRM/NewSections/Section3";
+import Section4 from "@/pages_sups/Products/BondifyCRM/NewSections/Section4";
 
 export default function Page() {
   const { lang, dir } = useSelector((state) => state.languageSlice);
@@ -75,6 +76,7 @@ export default function Page() {
         <Section1/>
         <Section2/>
         <Section3/>
+        <Section4/>
       </>
     </div>
   );
