a2283b5b1df9dd9988a72c64170f1eb66560a91e
israaboelfotouh <esraaaboelfotouh14@gmail.com>
2025-11-23 13:40:01 +0200

    Translate Create acc

--- src/pages_sups/Products/BondifyCRM/BondifyCRM/CreateAccount.jsx
+++ src/pages_sups/Products/BondifyCRM/BondifyCRM/CreateAccount.jsx
@@ -8,12 +8,16 @@ import * as Yup from "yup";
 import { toast } from "react-toastify";
 import { ToastContainer } from "react-toastify";
 
-
+import { useSelector } from "react-redux";
+import en from "@/stores/Language/en.json";
+import ar from "@/stores/Language/ar.json";
 
 export default function CreateAccount() {
   const [planOption, setPlanOption] = useState([])
 
-
+  const { lang, dir } = useSelector((state) => state.languageSlice);
+  const translations = { en, ar };
+  const section = translations[lang].BOUNDIFYCRM_CREATE_ACCOUNT;
   useEffect(() => {
     fetch("https://bondifycrm.com/api/bondify/plans")
       .then((res) => res.json())
@@ -41,39 +45,50 @@ export default function CreateAccount() {
       })
       .catch(() => setLoading(false));
   }, []);
-  const validationSchema = Yup.object({
-    full_name: Yup.string().required("Full Name is required"),
-    account_name: Yup.string()
-      .matches(/^[a-zA-Z0-9]+$/, "must only contain letters and numbers")
-      .required("Account Name is required"),
-    email_address: Yup.string()
-      .email("Email is invalid")
-      .required("Email is required"),
-    password: Yup.string()
-      .min(6, "Password must be a minimum of 6 characters")
-      .required("Password is required"),
-    plan: Yup.object().nullable().required("Plan is required"),
-  });
+const validationSchema = Yup.object({
+  full_name: Yup.string()
+    .required(lang === "ar" ? "الاسم الكامل مطلوب" : "Full Name is required"),
+
+  account_name: Yup.string()
+    .matches(
+      /^[a-zA-Z0-9]+$/,
+      lang === "ar" ? "يجب أن يحتوي على حروف وأرقام فقط" : "must only contain letters and numbers"
+    )
+    .required(lang === "ar" ? "اسم المستخدم مطلوب" : "Account Name is required"),
+
+  email_address: Yup.string()
+    .email(lang === "ar" ? "البريد الإلكتروني غير صالح" : "Email is invalid")
+    .required(lang === "ar" ? "البريد الإلكتروني مطلوب" : "Email is required"),
+
+  password: Yup.string()
+    .min(6, lang === "ar" ? "كلمة المرور يجب أن تكون 6 أحرف على الأقل" : "Password must be a minimum of 6 characters")
+    .required(lang === "ar" ? "كلمة المرور مطلوبة" : "Password is required"),
+
+  plan: Yup.object()
+    .nullable()
+    .required(lang === "ar" ? "الرجاء اختيار الباقة" : "Plan is required"),
+});
+
 
   return (
-    <div className="bondifycrm-page pt-[120px]">
+    <div className="bondifycrm-page pt-[120px] ">
       <Header />
 
-      <div className="w-full min-h-[80vh] flex flex-col md:flex-row items-center justify-center px-6 md:px-16 gap-10">
+      <div dir={dir} className="w-full min-h-[80vh] flex flex-col md:flex-row items-center justify-center px-6 md:px-16 gap-10">
 
         {/* Image */}
         <div className="w-full md:w-1/2 flex justify-center">
           <img
-            src="/images/create-account-illustration.png"
+            src="/images/CreatAcc.png"
             alt="Create Account"
-            className="w-[85%] max-w-[480px] object-contain"
+            className="w-[85%] max-w-[680px] object-contain"
           />
         </div>
 
         {/* Form */}
         <div className="w-full md:w-1/2">
-          <h2 className="text-3xl font-bold text-[#6772e5] ">Create Your Account</h2>
-          <p className="mb-[30px] text-[#797687]">Work through your task efficiently and intuitively.</p>
+          <h2 className="text-3xl font-bold text-[#6772e5] ">{section.title}</h2>
+          <p className="mb-[30px] text-[#797687]">{section.subtitle}</p>
 
           <Formik
             initialValues={{
@@ -97,9 +112,12 @@ export default function CreateAccount() {
                 
                 if (res.status === 200 ||res.status=== 201) {
                   // window.location.href = res.data.account_url;
-                        toast.success(
-          `Created Account successfully!`
-        );
+ toast.success(
+  lang === "ar" 
+    ? "تم إنشاء الحساب بنجاح!" 
+    : "Created Account successfully!"
+);
+
                 }
               })
               .catch((error) => {
@@ -115,51 +133,51 @@ export default function CreateAccount() {
           <Form className="flex flex-col gap-4">
 
             <div className="flex flex-col">
-              <label className="font-medium mb-1">Full Name</label>
+              <label className="font-medium mb-1">{section.full_name_label}</label>
               <Field
                 name="full_name"
                 type="text"
                 className="border rounded-lg p-3 outline-none focus:border-[#6772e5]"
-                placeholder="Enter your name"
+                 placeholder={section.full_name_placeholder}
               />
               <ErrorMessage name="full_name" component="div" className="text-red-500 text-sm mt-1" />
             </div>
 
             <div className="flex flex-col">
-              <label className="font-medium mb-1">User Name</label>
+              <label className="font-medium mb-1">{section.account_name_label}</label>
               <Field
                 name="account_name"
                 type="text"
                 className="border rounded-lg p-3 outline-none focus:border-[#6772e5]"
-                placeholder="@account_name"
+                placeholder={section.account_name_placeholder}
               />
               <ErrorMessage name="account_name" component="div" className="text-red-500 text-sm mt-1" />
             </div>
 
             <div className="flex flex-col">
-              <label className="font-medium mb-1">Email Address</label>
+              <label className="font-medium mb-1">{section.email_address_label}</label>
               <Field
                 name="email_address"
                 type="email_address"
                 className="border rounded-lg p-3 outline-none focus:border-[#6772e5]"
-                placeholder="Enter your email_address"
+                  placeholder={section.email_address_placeholder}
               />
               <ErrorMessage name="email_address" component="div" className="text-red-500 text-sm mt-1" />
             </div>
 
             <div className="flex flex-col">
-              <label className="font-medium mb-1">Password</label>
+              <label className="font-medium mb-1">{section.password_label}</label>
               <Field
                 name="password"
                 type="password"
                 className="border rounded-lg p-3 outline-none focus:border-[#6772e5]"
-                placeholder="Create a password"
+                 placeholder={section.password_placeholder}
               />
               <ErrorMessage name="password" component="div" className="text-red-500 text-sm mt-1" />
             </div>
 
             <div className="flex flex-col">
-              <label className="font-medium mb-1">Select plane</label>
+              <label className="font-medium mb-1">{section.plan_label}</label>
               <Select
                 options={planOption}
                 value={values.plan}
@@ -175,7 +193,7 @@ export default function CreateAccount() {
               type="submit"
               className="w-full bg-[#6772e5] hover:bg-[#5a63d8] text-white font-semibold py-3 rounded-lg mt-2 transition-all"
             >
-              Create Account
+               {section.create_account_button}
             </button>
 
           </Form>
