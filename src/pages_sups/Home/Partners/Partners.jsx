1fd4e68dbd3a68a467a4b55586e5ea4a4f2be16b
israaboelfotouh <esraaaboelfotouh14@gmail.com>
2025-11-30 10:41:31 +0200

    Edits

--- src/pages_sups/Home/Partners/Partners.jsx
+++ src/pages_sups/Home/Partners/Partners.jsx
@@ -7,32 +7,32 @@ import { Container } from "reactstrap";
 function Partners() {
   const { lang } = useSelector((state) => state.languageSlice);
   const { home } = useSelector((state) => state.homeRed);
-const logos = [
-  "partner-logo-1",
-  "partner-logo-2",
-  // "partner-logo-3",
-  "partner-logo-4",
-  "partner-logo-5",
-  "partner-logo-6",
-  "partner-logo-7",
-  "partner-logo-8",
-  "partner-logo-9",
-  "partner-logo-10",
-  "partner-logo-11",
-  "partner-logo-12",
-  "partner-logo-13",
-  // "partner-logo-14",
-  "partner-logo-15",
-];
+  const logos = [
+    "partner-logo-1",
+    "partner-logo-2",
+    // "partner-logo-3",
+    "partner-logo-4",
+    "partner-logo-5",
+    "partner-logo-6",
+    "partner-logo-7",
+    "partner-logo-8",
+    "partner-logo-9",
+    "partner-logo-10",
+    "partner-logo-11",
+    "partner-logo-12",
+    "partner-logo-13",
+    // "partner-logo-14",
+    "partner-logo-15",
+  ];
 
   const [page, setPage] = useState(0);
-  const logosPerPage = 8; 
+  const logosPerPage = 8;
   const totalPages = Math.ceil(logos.length / logosPerPage);
 
   useEffect(() => {
     const interval = setInterval(() => {
       setPage((prev) => (prev + 1) % totalPages);
-    }, 3000); 
+    }, 3000);
     return () => clearInterval(interval);
   }, [totalPages]);
 
@@ -40,6 +40,12 @@ const logos = [
     page * logosPerPage,
     page * logosPerPage + logosPerPage
   );
+  const safeLogos = [...currentLogos];
+
+while (safeLogos.length < 8) {
+  safeLogos.push(...currentLogos.slice(0, 8 - safeLogos.length));
+}
+
   return (
     <Container>
       <Section_head
@@ -47,28 +53,24 @@ const logos = [
         title_1={home(lang).sections.partners_title.title}
         des_1={home(lang).sections.partners_title.slogan}
       />
-<div className="flex flex-col gap-4 w-full max-w-5xl mx-auto mb-[100px]">
-  <div className="grid grid-cols-4 gap-6 justify-center">
-    {currentLogos.slice(0, 4).map((logo, idx) => (
-      <div
-        key={logo} 
-        className={`partner-logo ${logo} w-32 h-32 animate-logoChange`}
-        style={{ animationDelay: `${idx * 70}ms` }}
-      />
-    ))}
-  </div>
-
-
-  <div className="grid grid-cols-4 gap-2 justify-center">
-    {currentLogos.slice(4, 8).map((logo, idx) => (
-      <div
-        key={logo}
-        className={`partner-logo ${logo} w-32 h-32 animate-logoChange`}
-        style={{ animationDelay: `${(idx + 4) * 70}ms` }}
-      />
-    ))}
-  </div>
+      <div className="flex flex-col gap-4 w-full max-w-5xl mx-auto mb-[100px]">
+        <div className="logos-wrapper animate-slideGroup">
+          <div className=" mx-auto overflow-hidden py-12 bg-gray-50/50">
+           <div className="flex animate-slideLeftInfinite mb-12">
+  {[...safeLogos.slice(0, 4), ...safeLogos.slice(0, 4)].map((logo, idx) => (
+    <div key={`upper-${logo}-${idx}`} className={`partner-logo ${logo} flex-shrink-0 w-36 h-36 mx-20`} />
+  ))}
 </div>
+
+<div className="flex animate-slideRightInfinite">
+  {[...safeLogos.slice(4, 8), ...safeLogos.slice(4, 8)].map((logo, idx) => (
+    <div key={`lower-${logo}-${idx}`} className={`partner-logo ${logo} flex-shrink-0 w-36 h-36 mx-20`} />
+  ))}
+</div>
+
+          </div>
+        </div>
+      </div>
     </Container>
   );
 }
